a979489c849678a84595468b5ca1d47b738a1582
Anugerah <65181197+nugehood@users.noreply.github.com>
2020-07-25 16:11:10 +0800

    Added graphic

--- main.lua
+++ main.lua
@@ -1,7 +1,13 @@
+
+--Screen Resolution
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+
+
 function love.load()
+
+--Set the speed of paddle
 
   paddleSpeed = 255
 
@@ -9,6 +15,11 @@ function love.load()
   paddle1.y = 30
   paddle1.score = 0
 
+  paddle2 = {}
+  paddle2.y = 30
+  paddle2.score = 0
+
+--Setting the window
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     resizable = false,
@@ -20,6 +31,8 @@ end
 
 
 function love.update(dt)
+
+  --First Paddle Control Movement
   if love.keyboard.isDown('w') then
 
   paddle1.y = math.max(0, paddle1.y + -paddleSpeed * dt)
@@ -32,13 +45,36 @@ function love.update(dt)
     paddle1.y = math.min(650, paddle1.y + paddleSpeed * dt)
   end
 
+  --Second Paddle Control Movement
+  if love.keyboard.isDown('up') then
+    --body...
+
+    paddle2.y = math.max(0, paddle2.y + -paddleSpeed * dt)
+
+  end
+
+  if love.keyboard.isDown('down') then
+    --body...
+
+    paddle2.y = math.min(650, paddle2.y + paddleSpeed * dt)
+  end
+
 
 end
 
 function love.draw()
 
+  --Set background Color
+  love.graphics.clear(0.1490196078431373,0.1490196078431373,0.1490196078431373)
+
+
+  --First paddle graphic
   love.graphics.rectangle('fill', 10 ,paddle1.y, 30, 80)
 
-  
+  --First paddle graphic
+  love.graphics.rectangle('fill', 1235, paddle2.y, 30, 80)
+
+  --Ball graphic, set in center
+  love.graphics.circle('fill', WINDOW_WIDTH / 2 - 2, WINDOW_HEIGHT / 2 - 2, 10)
 
 end
