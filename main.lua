9d6622ca3663d64142517ed7cbc5e3292915197a
Anugerah <65181197+nugehood@users.noreply.github.com>
2020-07-25 15:58:00 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,44 @@
-function love.load()
-  paddle1 = {}
-  paddle1.y = 0;
-  paddle1.score = 0;
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
+function love.load()
+
+  paddleSpeed = 255
+
+  paddle1 = {}
+  paddle1.y = 30
+  paddle1.score = 0
+
+  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+  })
 
 
 end
 
-function love.update(dt)
 
+function love.update(dt)
+  if love.keyboard.isDown('w') then
+
+  paddle1.y = math.max(0, paddle1.y + -paddleSpeed * dt)
+
+  end
+
+  if love.keyboard.isDown('s') then
+    --body...
+
+    paddle1.y = math.min(650, paddle1.y + paddleSpeed * dt)
+  end
 
 
 end
 
 function love.draw()
 
+  love.graphics.rectangle('fill', 10 ,paddle1.y, 30, 80)
 
+  
 
 end
